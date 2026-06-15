2e2fee486f543384b8b05d1557f1a9b06fc76128
Gavin Binkley <gavingsb@me.com>
2026-06-14 22:12:34 -0400

    Restructured sandwich_maker.py and moved code to cashier.py

--- cashier.py
+++ cashier.py
@@ -3,11 +3,25 @@ class Cashier:
         pass
 
     def process_coins(self):
-        """Returns the total calculated from coins inserted.
-           Hint: include input() function here, e.g. input("how many quarters?: ")"""
-        ###
+        print("Please insert coins.")
+
+        large_dollars = int(input("How many large dollars?: "))
+        half_dollars = int(input("How many half dollars?: "))
+        quarters = int(input("How many quarters?: "))
+        nickels = int(input("How many nickels?: "))
+
+        total = large_dollars * 1.00
+        total += half_dollars * 0.50
+        total += quarters * 0.25
+        total += nickels * 0.05
+
+        return total
 
     def transaction_result(self, coins, cost):
-        """Return True when the payment is accepted, or False if money is insufficient.
-           Hint: use the output of process_coins() function for cost input"""
-        ##
+        if coins < cost:
+            print("Sorry there is not enough money. Money refunded.")
+            return False
+        else:
+            change = round(coins - cost, 2)
+            print(f"Here is ${change} in change.")
+            return True
